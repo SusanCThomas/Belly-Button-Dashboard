a09724b59369dcd0dcc09f0fe4ce6006bb914dac
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-24 01:52:09 -0500

    reorganized files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// // Step 1: Plotly
-// // 1. Use the D3 library to read in `samples.json`.
-// // Using d3.json() to fetch data from JSON samples file:
+// Step 1: Plotly
+// 1. Use the D3 library to read in `samples.json`.
+// Using d3.json() to fetch data from JSON samples file:
 var data;
 d3.json("data/samples.json").then((incomingData) => {
     console.log(incomingData);
@@ -16,7 +16,7 @@ d3.json("data/samples.json").then((incomingData) => {
         d3.select("#selDataset").append("option").text(name);
     });
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.     
+    // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.     
 
     // Initializes the page with a default plot
     function init() {
@@ -69,7 +69,7 @@ d3.json("data/samples.json").then((incomingData) => {
         // Plot the "bar" chart 
         Plotly.newPlot("bar", databar, layoutbar);
 
-// 3. Create a bubble chart that displays each sample.
+        // 3. Create a bubble chart that displays each sample.
         // Plot 2: Bubble Chart
         //  Create the Traces
         var trace2 = {
@@ -101,7 +101,7 @@ d3.json("data/samples.json").then((incomingData) => {
         // Plot the "bubble" chart 
         Plotly.newPlot('bubble', databubble, layoutbubble);
 
-g// 4. Display the sample metadata, i.e., an individual's demographic information.
+        // 4. Display the sample metadata, i.e., an individual's demographic information.
 
         // Filtering Demographic information 
         defaultDemographic = data.metadata.filter(sample => parseInt(sample.id) === 940)[0];
@@ -120,8 +120,8 @@ g// 4. Display the sample metadata, i.e., an individual's demographic informatio
             cell.text(`${key.toUpperCase()}: ${value}`);
         });
 
-// Advanced Challenge Assignment (Optional)
-// Please see bonus.js for alternative bonus gauge code matching given screenshot
+        // Advanced Challenge Assignment (Optional)
+        // Please see bonus.js for alternative bonus gauge code matching given screenshot
         // Plot 3: Gauge Chart
         var wfreq = defaultDemographic.wfreq;
         console.log(wfreq);
@@ -201,7 +201,7 @@ function optionChanged() {
     top10Ids = idOtuIds.slice(0, 10).reverse();
     top10Labels = idOtuLabels.slice(0, 10).reverse();
 
-// 6. Update all of the plots any time that a new sample is selected.
+    // 6. Update all of the plots any time that a new sample is selected.
     // Plot 1: Bar Chart
     Plotly.restyle("bar", "x", [top10Values]);
     Plotly.restyle("bar", "y", [top10Ids.map(outId => `OTU ${outId}`)]);
